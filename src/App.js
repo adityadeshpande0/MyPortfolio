7848e9dee663a10bf40c756f3d56cd5a66445cb2
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-01-27 20:22:21 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello I am Test 
+        Hello
         </p>
         <a
           className="App-link"
