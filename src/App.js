eac46ff52053b8db66e00b742c1c691b4e57a1f6
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-02-13 19:40:20 +0530

    Footer UI Improvement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import { RouteSharp } from "@mui/icons-material";
-import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import AlertDialogBox from "./Components/Popups/AlertDialogBox";
 import Routers from "./Navigation/Routers";
 
 function App() {
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import '..//Footer/footerStyles.css'
-import branchIcon from '..//..//Assets/Images/branch_icon_footer.svg'
+import React from "react";
+import "..//Footer/footerStyles.css";
+import branchIcon from "..//..//Assets/Images/branch_icon_footer.svg";
+import crossIcon from "..//..//Assets/Images/cross_icon_footer.svg";
+import alertIcon from "..//..//Assets/Images/alert_icon_footer.svg";
+import bellIcon from "..//..//Assets/Images/bell_icon_footer.svg";
+import { Stack } from "@mui/material";
 function Footer() {
   return (
-    <div className='main-block-footer' >
-      <div className='first-block-footer' >
-        <img src={branchIcon} />
-        <p>main</p>
+    <div className="main-block-footer">
+      <div className="second-parent-block-footer">
+        <Stack direction="row" spacing={1} className="first-block-footer">
+          <img src={branchIcon} />
+          <p>main</p>
+        </Stack>
+        <Stack direction="row" spacing={1} className="second-block-footer">
+          <img src={crossIcon} />
+          <p>0</p>
+          <img src={alertIcon} />
+          <p>0</p>
+        </Stack>
       </div>
+      <Stack direction="row" spacing={4} className="third-block-footer">
+        <p>Ln 17, Col 25</p>
+        <p>UTF-8</p>
+        <p>React JS</p>
+        <img src={bellIcon} />
+      </Stack>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
