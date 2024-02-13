7b6a33759ceba37463c916b91e1e0aec6488b957
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-02-13 11:31:16 +0530

    Alert Box Ui

--- src/Components/Popups/RecruiterAlert.jsx
+++ src/Components/Popups/RecruiterAlert.jsx
@@ -1,24 +1,23 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import React from "react";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import '..//Popups/recruiterAlertStyles.css'
+import "..//Popups/recruiterAlertStyles.css";
 function RecruiterAlert() {
   return (
     <Paper>
       <div className="top-block-alert">
         <InfoOutlinedIcon className="logo-alert" />
-        <Typography className="top-text-alert" >are you a recruiter ?</Typography>
+        <Typography className="top-text-alert">
+          are you a recruiter ?
+        </Typography>
       </div>
-      <div
-      className="bottom-block-alert"
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
-        <Typography className="bottom-text-alert">download my resume</Typography>
+      <div className="bottom-block-alert">
+        <Typography className="bottom-text-alert">
+          download my resume
+        </Typography>
         <div>
-          <Button className="button-alert" variant="contained" disableElevation="true">
-            yes
-          </Button>
-          <Button className="button-alert" disableElevation="true">no</Button>
+          <button className="button-alert">yes</button>
+          <button className="button-alert">no</button>
         </div>
       </div>
     </Paper>
