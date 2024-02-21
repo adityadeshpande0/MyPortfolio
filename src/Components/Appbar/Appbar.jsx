1c0569371b87dca6a9ada69b3b15b10b6ef652e8
Syntiaro-Dev <adityadeshpande1@outlook.com>
2024-02-21 20:21:10 +0530

    Routes Added

--- src/Components/Appbar/Appbar.jsx
+++ src/Components/Appbar/Appbar.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../Appbar/appbarStyles.css";
 import {
   FileCopyOutlined as FileCopyOutlinedIcon,
@@ -17,34 +18,42 @@ function Appbar() {
     setClickedIcon(index);
   };
   const icons = [
-    { Icon: FileCopyOutlinedIcon, index: 0 },
-    { Icon: GitHub, index: 1 },
-    { Icon: Code, index: 2 },
-    { Icon: Create, index: 3 },
-    { Icon: Email, index: 4 },
+    { Icon: FileCopyOutlinedIcon, index: 0, path: "/" },
+    { Icon: GitHub, index: 1, path: "/github" },
+    { Icon: Code, index: 2, path: "/projects" },
+    { Icon: Create, index: 3, path: "/blogs" },
+    { Icon: Email, index: 4, path: "/contact" },
   ];
   return (
     <Box className="main-block-appbar">
       <Box
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
-        {icons.map(({ Icon, index }) => (
-          <Icon
-            key={index}
-            className={`icon-appbar ${clickedIcon === index ? "clicked" : ""}`}
-            onClick={() => handleClick(index)}
-          />
+        {icons.map(({ Icon, index, path }) => (
+          <Link to={path} key={index}>
+            <Icon
+              key={index}
+              className={`icon-appbar ${
+                clickedIcon === index ? "clicked" : ""
+              }`}
+              onClick={() => handleClick(index)}
+            />
+          </Link>
         ))}
       </Box>
       <Box>
-        <AccountCircleOutlined
-          className={`icon-appbar ${clickedIcon === 5 ? "clicked" : ""}`}
-          onClick={() => handleClick(5)}
-        />
-        <Settings
-          className={`icon-appbar ${clickedIcon === 6 ? "clicked" : ""}`}
-          onClick={() => handleClick(6)}
-        />
+        <Link to="/about">
+          <AccountCircleOutlined
+            className={`icon-appbar ${clickedIcon === 5 ? "clicked" : ""}`}
+            onClick={() => handleClick(5)}
+          />
+        </Link>
+        <Link to="/settings">
+          <Settings
+            className={`icon-appbar ${clickedIcon === 6 ? "clicked" : ""}`}
+            onClick={() => handleClick(6)}
+          />
+        </Link>
       </Box>
     </Box>
   );
