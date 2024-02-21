523b749496cdd95041118480ba1ae1d7335c4f9b
Syntiaro-Dev <adityadeshpande1@outlook.com>
2024-02-21 21:32:57 +0530

    Styles Moved to relevent file

--- src/Navigation/Routers.js
+++ src/Navigation/Routers.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import '..//Navigation/routersStyles.css'
 import Home from "../Components/Home/Home";
 import Navbar from "../Components/Navbar/Navbar";
 import Appbar from "../Components/Appbar/Appbar";
@@ -8,12 +9,10 @@ import Footer from "../Components/Footer/Footer";
 function Routers() {
   return (
     <Router>
-      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <div className="main-block-router">
         <Navbar />
-        <div style={{ display: "flex", flexGrow: 1 }}>
+        <div className="content-block-router">
           <Appbar />
-
-          {/* Content rendered here */}
           <Routes>
             <Route exact path="/" element={<Home />} />
           </Routes>
