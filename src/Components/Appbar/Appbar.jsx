5f79d7a32ffd398d21befb32bda04c05a53e788e
Syntiaro-Dev <adityadeshpande1@outlook.com>
2024-02-21 18:39:25 +0530

    Appbar Changes

--- src/Components/Appbar/Appbar.jsx
+++ src/Components/Appbar/Appbar.jsx
@@ -1,19 +1,44 @@
-import { Stack } from '@mui/material'
-import React from 'react'
-
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import FileCopyOutlinedIcon from "@mui/icons-material/FileCopyOutlined";
+import GitHubIcon from "@mui/icons-material/GitHub";
 function Appbar() {
   return (
-    <aside>
-     <Stack>
-      <Stack>
-        
+    <Box
+      sx={{
+        height: "90.8vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#333333",
+        width: "40px",
+        alignItems: "center",
+      }}
+    >
+      <Stack spacing={2} direction="column">
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
       </Stack>
-      <Stack>
-
+      <Stack direction="column" spacing={2}>
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
+        <Box>
+          <FileCopyOutlinedIcon sx={{ color: "#fff" }} />
+        </Box>
       </Stack>
-     </Stack>
-    </aside>
-  )
+    </Box>
+  );
 }
 
-export default Appbar
+export default Appbar;
