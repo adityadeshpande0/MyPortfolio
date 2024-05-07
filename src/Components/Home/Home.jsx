db6389011cf3635b6b4c8a1c88adc49e549c2bf2
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-05-07 17:21:28 +0530

    Homepage Changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,10 @@ function Home() {
       <div className='home-container'>
         <p className='home-name-text'>Aditya Deshpande</p>
         <p className='type-anim-text'>Front End Developer</p>
+        <button>My Projects</button>
+        <button>Contact me</button>
       </div>
+      
     </div>
   )
 }
