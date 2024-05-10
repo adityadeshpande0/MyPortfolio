9e844d18c65caddfbd6afc43b9525ab8a8e43716
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-05-10 18:18:23 +0530

    About Me Page Changes

--- src/Components/MyProfile/Profile.jsx
+++ src/Components/MyProfile/Profile.jsx
@@ -8,24 +8,24 @@ function Profile() {
         "Proficient in React JS and React Native, I bring a wealth of knowledge and skills to any project, ensuring efficient problem-solving and top-notch results.",
     },
     {
-      title: "Expertise",
+      title: "Passion",
       description:
-        "Proficient in React JS and React Native, I bring a wealth of knowledge and skills to any project, ensuring efficient problem-solving and top-notch results.",
+        "Driven by a genuine love for development, I invest my energy into every task, guaranteeing dedication and exceptional outcomes.",
     },
     {
-      title: "Expertise",
+      title: "Collaboration",
       description:
-        "Proficient in React JS and React Native, I bring a wealth of knowledge and skills to any project, ensuring efficient problem-solving and top-notch results.",
+        "Valuing teamwork, I actively foster strong relationships with colleagues, contributing to a positive work environment and shared success.",
     },
     {
-      title: "Expertise",
+      title: "Innovation",
       description:
-        "Proficient in React JS and React Native, I bring a wealth of knowledge and skills to any project, ensuring efficient problem-solving and top-notch results.",
+        "Committed to staying ahead of industry trends, I consistently deliver cutting-edge solutions that stand the test of time.",
     },
   ];
 
   return (
-    <div className="socials-container">
+    <div className="aboutme-container">
       <div className="portfolio-intro">
         <h2 className="portfolio-intro__heading">
           &lt;!--Welcome to my portfolio!--!&gt;
@@ -38,17 +38,25 @@ function Profile() {
           functional user interfaces, and it has only grown stronger since then.
         </p>
       </div>
-      <p className="socials-item">
-        <span className="socials">const WhyHireMe ()=&gt; </span> &#91;
+      <p className="skills-item">
+        <span className="skill-functionheading">
+          <span style={{ color: "#59C3C3" }}>const</span> WhyHireMe=
+        </span>
+        &#91;
       </p>
       {dataSkills.slice(0, 5).map((item, index) => (
-        <p className="socials-item" key={index}>
+        <p className="skills-item" key={index}>
           &nbsp;&nbsp;&#123;&nbsp;
-          <strong>&quot;{item.title}&quot; : &quot;</strong>
-          {item.description}&quot;&#125;
+          <strong style={{ color: "#FFD166" }}>
+            <span style={{ color: "#a39c9c" }}>&quot;</span>
+            {item.title}
+            <span style={{ color: "#a39c9c" }}>&quot;:&quot;</span>
+          </strong>
+          {item.description}
+          <span style={{ color: "#a39c9c" }}>&quot;</span>&nbsp;&#125;&nbsp;,
         </p>
       ))}
-      <p className="socials-item">&#93;</p>
+      <p className="skills-item">&#93;</p>
     </div>
   );
 }
